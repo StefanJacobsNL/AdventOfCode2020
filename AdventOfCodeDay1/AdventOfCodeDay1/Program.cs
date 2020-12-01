4731aae64e8072aee578c318074cd58dfc6162b7
Stefan Jacobs <stefanjacobsrips@outlook.com>
2020-12-01 19:46:05 +0100

    Day 1 Part 1 & 2

---
+++ AdventOfCodeDay1/AdventOfCodeDay1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace AdventOfCodeDay1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = System.IO.File.ReadAllLines("input.txt");
+
+            int[] result = new int[input.Length];
+
+            int counter = 0;
+
+            foreach (var item in input)
+            {
+                result[counter] = int.Parse(item);
+                counter++;
+            }
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                for (int j = i++; j < result.Length; j++)
+                {
+                    for (int k = j++; k < result.Length; k++)
+                    {
+                        // if 2020 together then multiply 
+                        if (result[i] + result[j] + result[k] == 2020)
+                        {
+                            int endResult = result[i] * result[j] * result[k];
+
+                            System.Console.WriteLine($"The result: {result[i]} + {result[j]} + {result[k]} = 2020");
+                            System.Console.WriteLine($"The result: {result[i]} * {result[j]} * {result[k]} = {endResult}");
+                        }
+                    }
+                }
+            }            
+        }
+    }
+}
