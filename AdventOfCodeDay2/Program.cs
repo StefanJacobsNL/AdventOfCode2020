48480c86725470aa135963dfaee2156a7c0ef65d
Stefan Jacobs <stefanjacobsrips@outlook.com>
2020-12-02 20:06:47 +0100

    Day 2 part 1

--- AdventOfCodeDay2/Program.cs
+++ AdventOfCodeDay2/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
 
 namespace AdventOfCodeDay2
 {
@@ -6,7 +8,27 @@ namespace AdventOfCodeDay2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var input = File.ReadAllLines("InputDay2.txt");
+
+            int counter = 0;
+
+            int[] result = new int[input.Length];
+
+            foreach (var item in input)
+            {
+                var arraySplit = item.Split(new[] { '-', ' ', ':' }, StringSplitOptions.RemoveEmptyEntries);
+                
+                string letter = arraySplit[2];
+
+                int test = arraySplit[3].Count(x => x == arraySplit[2][0]);
+
+
+                if(test >= int.Parse(arraySplit[0]) && test <= int.Parse(arraySplit[1]))
+                {
+                    counter++;
+                }
+            }
+            Console.WriteLine($"The anwser is: {counter}");
         }
     }
 }
