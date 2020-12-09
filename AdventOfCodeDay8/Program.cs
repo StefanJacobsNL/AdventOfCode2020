d8575c0f09f8c7dd62919644afd15e83761636b8
Stefan Jacobs <stefanjacobsrips@outlook.com>
2020-12-09 19:01:01 +0100

    Day 8 Part 2

--- AdventOfCodeDay8/Program.cs
+++ AdventOfCodeDay8/Program.cs
@@ -23,21 +23,18 @@ namespace AdventOfCodeDay8
             {
                 if (usedInstructions.Where(x => x == i).Count() == 0)
                 {
+                    usedInstructions.Add(i);
                     if (rules[i].Contains("acc"))
                     {
-                        counter += GetAcc(rules[i]);
+                        counter += GetValue(rules[i]);
                     }
                     else if (rules[i].Contains("jmp"))
                     {
-                        i += GetJmp(rules[i]);
+                        i += GetValue(rules[i]);
 
                         i--;
                     }
-                    else if (rules[i].Contains("nop"))
-                    {
-                    }
 
-                    usedInstructions.Add(i);
                 }
                 else
                 {
@@ -46,38 +43,93 @@ namespace AdventOfCodeDay8
             }
 
             Console.WriteLine($"The answer of part 1 is: { counter } ");
+
+            counter = 0;
+            usedInstructions.Clear();
+
+
+            for (int i = 0; i < rules.Count; i++)
+            {
+                if (rules[i].Contains("jmp"))
+                {
+                    rules[i] = rules[i].Replace("jmp", "nop");
+                }
+                else if (rules[i].Contains("nop"))
+                {
+                    rules[i] = rules[i].Replace("nop", "jmp");
+                }
+
+                if (CheckList(rules) == 0)
+                {
+                    if (rules[i].Contains("jmp"))
+                    {
+                        rules[i] = rules[i].Replace("jmp", "nop");
+                    }
+                    else if (rules[i].Contains("nop"))
+                    {
+                        rules[i] = rules[i].Replace("nop", "jmp");
+                    }
+                }
+                else
+                {
+                    var test = rules[i];
+                    
+                    counter = CheckList(rules);
+                    break;
+                }
+            }
+
+            Console.WriteLine($"The answer of part 2 is: { counter } ");
         }
 
-        public static int GetAcc(string instruction)
+        public static int CheckList(List<string> rules)
         {
             int counter = 0;
+            int currentInstruction = 0;
+            var usedInstructions = new List<int>();
 
-            if (instruction.Split(" ")[1].Contains("+"))
+            for (int i = 0; i < rules.Count; i++)
             {
-                counter += int.Parse(instruction.Split(" +")[1]);
-            }
-            else if (instruction.Split(" ")[1].Contains("-"))
-            {
-                counter -= int.Parse(instruction.Split(" -")[1]);
-            }
+                if (usedInstructions.Any(x => x == i) == false)
+                {
+                    usedInstructions.Add(i);
 
-            return counter;
+                    if (rules[i].Contains("acc"))
+                    {
+                        counter += GetValue(rules[i]);
+                    }
+                    else if (rules[i].Contains("jmp"))
+                    {
+                        i += GetValue(rules[i]);
+
+                        i--;
+
+                        if (i < 0)
+                        {
+                            return 0;
+                        }
+                    }
+                }
+                else
+                {
+                    return 0;
+                }
+
+                currentInstruction = i;
+            }
+            if (currentInstruction +1 == rules.Count)
+            {
+                return counter;
+            }
+            else
+            {
+                return 0;
+            }
         }
 
-        public static int GetJmp(string instruction)
+        public static int GetValue(string instruction)
         {
-            int counter = 0;
-
-            if (instruction.Split(" ")[1].Contains("+"))
-            {
-                counter += int.Parse(instruction.Split(" +")[1]);
-            }
-            else if (instruction.Split(" ")[1].Contains("-"))
-            {
-                counter -= int.Parse(instruction.Split(" -")[1]);
-            }
-
-            return counter;
+            return int.Parse(instruction.Split(" ")[1]);
         }
     }
 }
