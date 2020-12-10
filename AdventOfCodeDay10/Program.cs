3a6e3c3bf6b3ba89276dfae07549dd79de344cde
Stefan Jacobs <stefanjacobsrips@outlook.com>
2020-12-10 20:29:26 +0100

    Day 10 Part 2

--- AdventOfCodeDay10/Program.cs
+++ AdventOfCodeDay10/Program.cs
@@ -10,28 +10,33 @@ namespace AdventOfCodeDay8
         static void Main(string[] args)
         {
             var input = File.ReadAllLines("input.txt");
-            var numbers = new List<int>();
+            var numbers = new List<long>();
             long answerPart1 = 0;
-            int joltage = 0;
+            long answerPart2 = 0;
+            long joltage = 0;
             
 
             foreach (var item in input)
             {
-                numbers.Add(int.Parse(item));
+                numbers.Add(long.Parse(item));
             }
+
+            numbers.Sort();
 
             answerPart1 = CheckList(numbers, joltage);
 
             Console.WriteLine($"The answer of part 1 is: { answerPart1 } ");
+
+            answerPart2 = CheckAllWays(numbers);
+
+            Console.WriteLine($"The answer of part 2 is: { answerPart2 } ");
         }
 
-        public static int CheckList(List<int> numbers, int joltage)
+        public static long CheckList(List<long> numbers, long joltage)
         {
-            int answer = 0;
-            int difference1 = 0;
-            int difference3 = 1;
-
-            numbers.Sort();
+            long answer = 0;
+            long difference1 = 0;
+            long difference3 = 1;
 
             foreach (var item in numbers)
             {
@@ -53,6 +58,54 @@ namespace AdventOfCodeDay8
 
             answer = difference1 * difference3;
 
+            return answer;
+        }
+
+        public static long CheckAllWays(List<long> adapters)
+        {
+            long answer = 0;
+            var options = new Dictionary<long, long>() { { 0, 1 } };
+            var newOptions = new Dictionary<long, long>();
+
+            while (options.Any())
+            {
+                foreach (var option in options)
+                {
+                    foreach (var adapter in adapters)
+                    {
+                        if (adapter > option.Key)
+                        {
+                            if (adapter <= 3 + option.Key)
+                            {
+                                if (!newOptions.ContainsKey(adapter))
+                                {
+                                    newOptions.Add(adapter, option.Value);
+                                }
+                                else
+                                {
+                                    newOptions[adapter] += option.Value;
+                                }
+                            }
+                        }
+                    }
+                }
+
+                options.Clear();
+
+                foreach (var option in newOptions)
+                {
+                    if (option.Key != adapters.Last())
+                    {
+                        options.Add(option.Key, option.Value);
+                    }
+                    else
+                    {
+                        answer += option.Value;
+                    }
+                }
+
+                newOptions.Clear();
+            }
             return answer;
         }
     }
