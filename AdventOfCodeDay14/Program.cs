bf237173a981c4164c6a14a0df717dbece0079c2
Stefan Jacobs <stefanjacobsrips@outlook.com>
2020-12-14 21:56:28 +0100

    Day 14 Part 1

--- AdventOfCodeDay14/Program.cs
+++ AdventOfCodeDay14/Program.cs
@@ -12,41 +12,63 @@ namespace AdventOfCodeDay14
             var input = File.ReadAllLines("input.txt").ToList();
             long answerPart1 = 0;
 
-            var binaryt = input[2].Split(new[] { "mem[7] =" }, StringSplitOptions.RemoveEmptyEntries);
-
-            //byte binary = Convert.ToByte(int.Parse(binaryt[0]));
-
-            var asdsa = input[0].Split(new[] { "mask = " }, StringSplitOptions.RemoveEmptyEntries);
-
-            var eeee = asdsa[0].Length;
-
             answerPart1 = SumOfAllValues(input);
 
             Console.WriteLine($"The answer of part 1 is: { answerPart1 } ");
         }
 
-        public static int SumOfAllValues(List<string> input)
+        public static long SumOfAllValues(List<string> input)
         {
-            int answer = 0;
+            long answer = 0;
+            long memoryKey = 0;
+            long memoryValue = 0;
             string mask = "";
             string result = "";
-
-            result = Convert.ToString(11, 2);
-            Console.WriteLine(result);
+            var memory = new Dictionary<long, long>();
 
             foreach (var item in input)
             {
                 if (item.Contains("mask"))
                 {
-                    var splitItems = item.Split(new[] { "mask =" }, StringSplitOptions.RemoveEmptyEntries);
-                    mask = splitItems[0];
+                    var splitMask = item.Split(new[] { "mask = " }, StringSplitOptions.RemoveEmptyEntries);
+                    mask = splitMask[0];
                 }
                 else if (item.Contains("mem"))
                 {
+                    var splitMemory = item.Split(new[] { "mem[", "] = " }, StringSplitOptions.RemoveEmptyEntries);
+                    result = Convert.ToString(int.Parse(splitMemory[1]), 2);
+                    memoryKey = Convert.ToInt64(splitMemory[0]);
 
+                    char[] charArray = result.PadLeft(36, '0').ToCharArray();
+
+                    for (int i = 0; i < mask.Length; i++)
+                    {
+                        if (mask[i] == '1')
+                        {
+                            charArray[i] = '1';
+                        }
+                        else if (mask[i] == '0')
+                        {
+                            charArray[i] = '0';
+                        }
+                    }
+
+                    result = new string(charArray);
+
+                    memoryValue = Convert.ToInt64(result, 2);
+
+                    if (!memory.ContainsKey(memoryKey))
+                    {
+                        memory.Add(memoryKey, memoryValue);
+                    }
+                    else
+                    {
+                        memory[memoryKey] = memoryValue;
+                    }
                 }
             }
 
+            answer = memory.Values.Sum();
 
             return answer;
         }
