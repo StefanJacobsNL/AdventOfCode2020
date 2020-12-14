eab1e67e15d009bd2b5e0b8eedb4f0a93c87cb49
Stefan Jacobs <stefanjacobsrips@outlook.com>
2020-12-14 22:19:56 +0100

    Updated Part 1

--- AdventOfCodeDay14/Program.cs
+++ AdventOfCodeDay14/Program.cs
@@ -10,20 +10,13 @@ namespace AdventOfCodeDay14
         static void Main(string[] args)
         {
             var input = File.ReadAllLines("input.txt").ToList();
-            long answerPart1 = 0;
 
-            answerPart1 = SumOfAllValues(input);
-
-            Console.WriteLine($"The answer of part 1 is: { answerPart1 } ");
+            Console.WriteLine($"The answer of part 1 is: { SumOfAllValuesPart1(input) } ");
         }
 
-        public static long SumOfAllValues(List<string> input)
+        public static long SumOfAllValuesPart1(List<string> input)
         {
-            long answer = 0;
-            long memoryKey = 0;
-            long memoryValue = 0;
             string mask = "";
-            string result = "";
             var memory = new Dictionary<long, long>();
 
             foreach (var item in input)
@@ -36,26 +29,22 @@ namespace AdventOfCodeDay14
                 else if (item.Contains("mem"))
                 {
                     var splitMemory = item.Split(new[] { "mem[", "] = " }, StringSplitOptions.RemoveEmptyEntries);
-                    result = Convert.ToString(int.Parse(splitMemory[1]), 2);
-                    memoryKey = Convert.ToInt64(splitMemory[0]);
+                    string result = Convert.ToString(int.Parse(splitMemory[1]), 2);
+                    long memoryKey = Convert.ToInt64(splitMemory[0]);
 
                     char[] charArray = result.PadLeft(36, '0').ToCharArray();
 
                     for (int i = 0; i < mask.Length; i++)
                     {
-                        if (mask[i] == '1')
+                        if (mask[i] == '1' || mask[i] == '0')
                         {
-                            charArray[i] = '1';
-                        }
-                        else if (mask[i] == '0')
-                        {
-                            charArray[i] = '0';
+                            charArray[i] = mask[i];
                         }
                     }
 
                     result = new string(charArray);
 
-                    memoryValue = Convert.ToInt64(result, 2);
+                    long memoryValue = Convert.ToInt64(result, 2);
 
                     if (!memory.ContainsKey(memoryKey))
                     {
@@ -68,9 +57,7 @@ namespace AdventOfCodeDay14
                 }
             }
 
-            answer = memory.Values.Sum();
-
-            return answer;
+            return memory.Values.Sum();
         }
     }
 }
