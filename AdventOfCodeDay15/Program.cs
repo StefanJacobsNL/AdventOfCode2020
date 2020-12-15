fa78e60cfc1578cf46f0b1a0f2b43d15874fc0e8
Stefan Jacobs <stefanjacobsrips@outlook.com>
2020-12-15 22:44:37 +0100

    Day 15 Part 2

--- AdventOfCodeDay15/Program.cs
+++ AdventOfCodeDay15/Program.cs
@@ -10,60 +10,57 @@ namespace AdventOfCodeDay15
         static void Main(string[] args)
         {
             var input = File.ReadLines("input.txt").ToList();
-            List<int> numbers = new List<int>();
+            List<long> numbers = new List<long>();
+            long firstRound = 2020;
+            long secondRound = 30000000;
 
             foreach (var item in input[0].Split(','))
             {
-                numbers.Add(int.Parse(item));
+                numbers.Add(long.Parse(item));
             }
 
+            Console.WriteLine($"The answer of part 1 is: { NumberGame(numbers, firstRound) } ");
 
-            Console.WriteLine($"The answer of part 1 is: { NumberGame(numbers) } ");
+            Console.WriteLine($"The answer of part 2 is: { NumberGame(numbers, secondRound) } ");
         }
 
-        public static int NumberGame(List<int> numbers)
+        public static long NumberGame(List<long> numbers, long rounds)
         {
-            int answer = numbers[0];
-            int index = 0;
-            List<int> usedNumbers = new List<int>();
+            long answer = numbers[0];
+            int counter = 0;
+            var numberDictionary = new Dictionary<long, long>();
 
-            for (int i = 0; i < 2020; i++)
+            foreach (var item in numbers)
             {
-                if (index < numbers.Count)
+                numberDictionary.Add(item, counter);
+                answer = item;
+                counter++;
+            }
+
+            long lastIndex = numbers.Count() - 1;
+
+            for (int index = numbers.Count; index < rounds; index++)
+            {
+                if (numberDictionary.ContainsKey(answer) && numberDictionary[answer] == lastIndex)
                 {
-                    if (!usedNumbers.Contains(numbers[index]))
-                    {
-                        usedNumbers.Add(numbers[index]);
-                        answer = numbers[index];
-                        index++;
-                    }
-                }
-                else if (usedNumbers.Where(x => x == answer).Count() == 1)
-                {
-                    usedNumbers.Add(answer);
-                    usedNumbers.Add(0);
-                    numbers.Add(0);
                     answer = 0;
-                    index++;
+                }
+                else if (!numberDictionary.ContainsKey(answer))
+                {
+                    numberDictionary.Add(answer, index);
                 }
                 else
                 {
-                    var allIndexes = numbers.Select((item, index) => new { itemName = item, position = index }).Where(i => i.itemName == answer).ToList();
-
-                    int lastNumber = allIndexes.Count() - 1;
-
-                    int lastIndex = allIndexes[lastNumber].position + 1;
-                    int secondLastIndex = allIndexes[lastNumber - 1].position + 1;
-
-                    answer = lastIndex - secondLastIndex;
-                    numbers.Add(answer);
-                    usedNumbers.Add(answer);
-                    index++;
-                    
+                    var earlierIndex = numberDictionary[answer] + 1;
+                    numberDictionary[answer] = index - 1;
+                    answer = index - earlierIndex;
+                    if (!numberDictionary.ContainsKey(answer))
+                    {
+                        numberDictionary.Add(answer, index);
+                    }
                 }
+                lastIndex = index;
             }
-
-            answer = numbers.Last();
 
             return answer;
         }
